673d80e5276268fe77fc0811d399050c9463bd2e
jinti <shenjindi@fourz.cn>
2023-03-21 14:57:33 +0800

    remove dev server in backend.js

--- ui/src/backend.js
+++ ui/src/backend.js
@@ -1,11 +1,11 @@
-const serveraddr = 'http://localhost:8000'
+
 export async function getSummary() {
-    let req = await fetch(`${serveraddr}/api/summary`, { method: 'POST' })
+    let req = await fetch(`/api/summary`, { method: 'POST' })
     return await req.json()
 }
 
 export async function listMails(pos, keyword, limit = 20) {
-    let req = await fetch(`${serveraddr}/api/mail/query`, {
+    let req = await fetch(`/api/mail/query`, {
         method: 'POST',
         body: JSON.stringify({
             pos, keyword, limit,
@@ -16,7 +16,7 @@ export async function listMails(pos, keyword, limit = 20) {
 }
 
 export async function markMailOpened(msgid, opened) {
-    let req = await fetch(`${serveraddr}/api/mail/${msgid}`, {
+    let req = await fetch(`/api/mail/${msgid}`, {
         method: 'PATCH',
         body: JSON.stringify({
             opened
@@ -26,7 +26,7 @@ export async function markMailOpened(msgid, opened) {
 }
 
 export async function deleteMail(msgid) {
-    let req = await fetch(`${serveraddr}/api/mail/${msgid}`, {
+    let req = await fetch(`/api/mail/${msgid}`, {
         method: 'DELETE'
     })
     return await req.json()
